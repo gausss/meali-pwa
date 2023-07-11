1b2bdb4eb7dfa7424bc53800dfce3a07da6e80d5
gausss <gausssven@gmail.com>
2023-07-11 17:05:04 +0200

    Add meal service tests

---
+++ src/app/shared/meal.service.spec.ts
@@ -0,0 +1,35 @@
+import { MealService } from './meal.service';
+import { Difficulty, Ingredient } from '../meal/meal.model';
+
+describe('MealService', () => {
+  let mealService: MealService;
+  beforeEach(() => {
+    mealService = new MealService();
+  });
+
+  it('should have default meals', () => {
+    expect(mealService.getAllIds()).toHaveLength(6);
+    let meal = mealService.get(2);
+    expect(meal?.name).toBe('Farfalle');
+    expect(meal?.difficulty).toBe('easy');
+  });
+
+  it('should delete by id', () => {
+    mealService.delete(2);
+    expect(mealService.getAllIds()).toHaveLength(5);
+    let meal = mealService.get(2);
+    expect(meal).toBeUndefined();
+  });
+
+  it('should create with new id', () => {
+    mealService.create({
+      id: 0,
+      name: 'MealNew',
+      difficulty: 'hard',
+    });
+    expect(mealService.getAllIds()).toHaveLength(6);
+    let meal = mealService.get(7);
+    expect(meal?.name).toBe('MealNew');
+    expect(meal?.difficulty).toBe('hard');
+  });
+});
